9f0c474058c784d0073cef1d371ee4436fad23c1
Aryan-kinge <44639863+Aryan-kinge@users.noreply.github.com>
2019-02-02 15:11:58 +0000

    latest project eueler

---
+++ Project Euler/Problem 5.py
@@ -0,0 +1,15 @@
+num = []
+
+
+def modular(number):
+    access = number
+    while len(num) < 5:
+        if access % 1 and 2 and 3 and 4 and 5 and 6 and 7 and 8 and 90 and 10 and 11 and 12 and 13 and 14 and 15 and 16 and 17 and 18 and 19 and 20 == 0:
+            num.append(access)
+        else:
+            access += 1
+
+
+modular(1)
+print(min(num))
+
